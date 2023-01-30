899eb0fed81158bd4da3e09b91d1346ebef9f29f
k2lighter <k2lighter@gmail.com>
2023-01-31 00:17:05 +0300

    added checking.py, and update test_create_new_book method

--- tests/test_library_api.py
+++ tests/test_library_api.py
@@ -1,13 +1,35 @@
+from utils.checking import Checking
 from utils.api import Library_api
 
 
 class Test_create_book:
-    """Создание, изменение и удаление новой книги"""
+    """ Тест на создание, изменение и удаление новой книги"""
 
     def test_create_new_book(self):
-        print('Method POST')
+        print('Method POST - метод создание новой книги')
         result_post = Library_api.create_new_book()
         check_post = result_post.json()
         book_id = str(check_post.get("book")["id"])  # получаем id новой книги
+        Checking.check_status_code(result_post, 201)
 
+        print("Method GET after POST - для проверки создания новой книги")
+        result_get = Library_api.get_new_book(book_id)
+        check_get = list(result_get.json()["book"])  # получаем список ключей словаря "book"
+        Checking.check_status_code(result_get, 200)
+        Checking.check_value(check_get, 'name')
 
+        print("Method PUT - метод для изменения новой книги")
+        result_put = Library_api.put_new_book(book_id)
+        Checking.check_status_code(result_get, 200)
+
+        print("Method GET after PUT для проверки изменения инфы о новой книге")
+        result_get = Library_api.get_new_book(book_id)
+        Checking.check_status_code(result_get, 200)
+
+        print("Method DELETE для удаления новой книги")
+        result_delete = Library_api.delete_new_book(book_id)
+        Checking.check_status_code(result_get, 200)
+
+        print("Method GET after DELETE для проверки удаления новой книги")
+        result_get = Library_api.delete_new_book(book_id)
+        Checking.check_status_code(result_get, 404)
--- utils/checking.py
+++ utils/checking.py
@@ -9,29 +9,16 @@ class Checking:
     def check_status_code(response: Response, status_code):
         """Метод для проверки статуса кода"""
         assert status_code == response.status_code
-        if response.status_code == status_code:
-            print(f"TEST PASSED. Status code: {response.status_code}")
-
-    @staticmethod
-    def check_json_token(response: Response, expected_value):
-        """Метод для проверки наличия обязательных полей в ответах запроса"""
-        token = json.loads(response.text)
-        assert list(token) == expected_value
-        print('All columns are in place')
-
-    @staticmethod
-    def check_json_value_token(response: Response, field_name, expected_value):
-        """Метод для проверки значений обязательных полей в ответе запроса"""
-        check = response.json()
-        check_info = check.get(field_name)
-        assert check_info == expected_value
-        print(f'test: PASSED. {field_name} equal')
-
-    @staticmethod
-    def check_json_search_word_in_value_token(response: Response, field_name, search_word):
-        check = response.json()
-        check_info = check.get(field_name)
-        if search_word in check_info:
-            print(f'Word {search_word} in')
+        if status_code == response.status_code:
+            print(f"Успех, тест пройден!!! Статус код = {response.status_code}")
         else:
-            print('Word {search_word} out')
+            print(f"Провал, тест не пройден!!! Статус код = {response.status_code}")
+
+    @staticmethod
+    def check_value(keys_list, expected_value):
+        """Метод для проверки наличия обязательных полей в ответах запроса"""
+
+        if expected_value in keys_list:
+            print(f"Успех, тест пройден!!! Обязательное поле {expected_value} присутствует")
+        else:
+            raise AssertionError(f"Провал, тест не пройден!!! Поле {expected_value} отсутствует")
